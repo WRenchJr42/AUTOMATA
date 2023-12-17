32c2b07e5697f0a642e19c480f93610640757fcc
Yashas P <130992790+WRenchJr42@users.noreply.github.com>
2023-12-17 09:19:35 +0530

    Update AUTOMATA.py

--- AUTOMATA.py
+++ AUTOMATA.py
@@ -1,10 +1,6 @@
 import urllib.error
 from urllib import request as rq
 import sys
-import ssl
-from urllib.parse import urlparse
-from http.cookiejar import CookieJar
-
 
 class colors:
     green = '\033[92m'
@@ -36,23 +32,7 @@ class funny:
                                   "\n               h t t p s : / / f a m b o o k . c o m "
                                   "\nPlease mention https ,http ,ftp or other protocols for this to work....")
             self.url = str(input(colors.green + "\nEnter a URL : "))
-            self.mode = int(input(colors.purple + "\nSet of options : " +
-                                  colors.cyan + "\n\n 0 : Check Connection " +
-                                  "\n 1 : GET ; Response Code" +
-                                  "\n 2 : GET ; Byte Length"
-                                  "\n 3 : GET ; Peek at the response"
-                                  "\n 4 : GET ; Decode "
-                                  "\n 5 : GET Cookies "
-                                  "\n 6 : GET Headers "
-                                  "\n 7 : PARSE"
-                                  "\n 8 :POST and GET ; SEARCH Something in SearchBar"
-                                  "\n 9 : EXIT\n\n" +
-                                  colors.green + "Your input : "))
-
-            if self.mode > int(10) or self.mode < int(-1):
-                print(colors.warning + "\n ERROR : Invalid Input")
-                print(colors.fail + "\n A.O.U.T.O.M.A.T.A FAILED")
-                sys.exit(1)
+            
 
         except Exception as x:
             print(f'ERROR : {x}')
@@ -64,57 +44,7 @@ class funny:
 
         error_occurred = False
         while self.mode != int(9):
-            try:
-                context = ssl._create_unverified_context()
-
-                ssl_response = rq.urlopen(self.url, context=context)
-
-                if self.mode == int(0):
-                    response = rq.urlopen(self.url)
-                    print(f'\n Is URL closed? : {response.closed}')
-
-                elif self.mode == int(1):
-                    read = rq.urlopen(self.url)
-                    print(f'\nResponse Code : {read.getcode()}')
-
-                elif self.mode == int(2):
-                    read = rq.urlopen(self.url)
-                    print(f'\nByte Length : {len(read.read())}')
-
-                elif self.mode == int(3):
-                    read = rq.urlopen(self.url)
-                    print(f'\nPeek URL : {read.peek()}')
-
-                elif self.mode == int(4):
-                    read = rq.urlopen(self.url)
-                    data = read.read()
-                    html = data.decode("UTF-8")
-                    print(html)
-
-                elif self.mode == int(5):
-                    cj = CookieJar()
-                    opener = rq.build_opener(rq.HTTPCookieProcessor(cj))
-                    opener.open(self.url)
-                    cookies = list(cj)
-                    print(f"\nNumber of Cookies : {len(cj)}")
-                    for i in cookies:
-                        print(i)
-
-                elif self.mode == int(6):
-                    read = rq.urlopen(self.url)
-                    z = read.getheaders()
-                    print("\nHeaders : \n")
-                    for i in z:
-                        print(i)
-
-                elif self.mode == int(7):
-                    parsed_url = urlparse(self.url)
-                    print("\nProtocol : ", parsed_url.scheme)
-                    print("Network Location : ", parsed_url.netloc)
-                    print("Path : ", parsed_url.path)
-                    print("Query : ", parsed_url.query)
-
-                elif self.mode == int(8):
+            try:    
                     elements = {}
                     print(colors.purple + "There are two things that are pivotal in this case:" +
                           colors.cyan + "\n 3 : The path"
@@ -173,27 +103,6 @@ class funny:
                     print(colors.fail + f"\nURL Error: {e}")
                     print(colors.warning + "\nURL Reason : ", e.reason)
                     error_occurred = True
-
-                    continue_option = input(colors.purple + "\nDo you want to continue? (y/n): ").lower()
-                    if continue_option != 'y':
-                        break
-
-            self.mode = int(input(colors.purple + "\nSet of options : " +
-                                  colors.cyan + "\n\n 0 : Check Connection " +
-                                  "\n 1 : GET ; Response Code" +
-                                  "\n 2 : GET ; Byte Length"
-                                  "\n 3 : GET ; Peek at the response"
-                                  "\n 4 : GET ; Decode "
-                                  "\n 5 : GET Cookies "
-                                  "\n 6 : GET Headers "
-                                  "\n 7 : PARSE"
-                                  "\n 8 : SEARCH Something in SearchBar"
-                                  "\n 9 : EXIT\n\n" +
-                                  colors.green + "Your input : "))
-
-        if self.mode == int(9):
-            print(colors.warning + "\n Exiting A.U.T.O.M.A.T.A")
-            sys.exit(0)
 
 
 if __name__ == '__main__':
